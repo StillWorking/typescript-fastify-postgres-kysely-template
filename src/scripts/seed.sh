8df19f3b90ed6490f38aadb7272461795633b78b
Taha Sheikh <taha.sheikh@timedochealth.com>
2023-03-28 13:10:36 -0400

    Fixed seed file

--- src/scripts/seed.sh
+++ src/scripts/seed.sh
@@ -4,10 +4,10 @@
 mkdir -p dist
 
 # Compile the TypeScript files and output them to the dist directory
-npx tsc --resolveJsonModule --esModuleInterop --outDir dist src/db/seeds/seed.ts
+npx tsc --resolveJsonModule --esModuleInterop --outDir dist/db src/db/seeds/seed.ts
 
 # Run the seed.js script from the dist directory
-node dist/seeds/seed.js
+node dist/db/seeds/seed.js
 
 # Delete the dist directory and all of its contents
 rm -rf dist
